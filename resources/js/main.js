739a9f4e6990e9f081a236384163144c41d5c582
Jorg van der Schaaf <JVDS@MacBook-Air-van-Jorg-3958.local>
2017-06-20 00:57:12 +0200

    hide navigation bar part 2 of 2

--- resources/js/main.js
+++ resources/js/main.js
@@ -15,7 +15,11 @@ $(document).ready(function(){
     if (y - x > 50){
       var z = $('.navbar').css('height');
       $('.navbar').animate({top: '-' + z}, 150);
-    })
-});
+      x = y;
+    } else if (x - y > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      x = y;
+    }
+  });
 
 });
