8fabb5e71a9bc0e2c92bde8a0392751d13aae33d
Jorg van der Schaaf <JVDS@MacBook-Air-van-Jorg-3958.local>
2017-06-20 00:16:14 +0200

    commit with all styling with media max 768px but without fix for the carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
   $('.carousel').slick({
+    slidesToScroll: 1,
     autoplay: true,
     autoplayspeed: 4000,
     arrows: false,
     draggable: false,
     pauseOnFocus: false,
-    pauseOnHover: false    
+    pauseOnHover: false
   });
 });
